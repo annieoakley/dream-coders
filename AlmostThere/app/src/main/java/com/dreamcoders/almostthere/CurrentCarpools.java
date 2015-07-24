dc21419025423d8de8563f5d4bac67baae80ea04
gdomergue <gdomergu@callutheran.edu>
2015-07-23 20:36:51 -0700

    Create CurrentCarpools.java

---
+++ AlmostThere/app/src/main/java/com/dreamcoders/almostthere/CurrentCarpools.java
@@ -0,0 +1,17 @@
+package com.dreamcoders.almostthere;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+* Created by ginadomergue on 7/23/15.
+*/
+public class CurrentCarpools extends Activity {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        // ToDo add your GUI initialization code here
+
+    }
+}
